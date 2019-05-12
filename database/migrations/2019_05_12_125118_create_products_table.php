33ef9bfff359115244a8db7980e011d94086ba62
razatt <razatt23@gmail.com>
2019-05-12 19:57:13 +0600

    Add product migrate

---
+++ database/migrations/2019_05_12_125118_create_products_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('cat')->index();
+            $table->string('vendor_code');
+            $table->string('barcode');
+            $table->string('collection');
+            $table->tinyInteger('sex', 1);
+            $table->text('desc');
+            $table->string('img');
+
+            $table->tinyInteger('size', 2);
+            $table->string('num_sizes');
+            $table->string('composition');
+            $table->string('age');
+            $table->string('color');
+
+            $table->integer('quantity');
+            $table->integer('unit');
+            $table->integer('pack_price');
+            $table->integer('price');
+            $table->integer('tax');
+            $table->integer('available');
+            $table->boolean('status')->default(true)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
